486ca3151db2af383cf5a8f474ba5fd8bde5335f
sariya <dayzwaitik123@yandex.ru>
2024-07-08 17:56:13 +0300

    register models in admin

--- courses/admin.py
+++ courses/admin.py
@@ -1,1 +1,21 @@
-# Register your models here.
+from django.contrib import admin
+from .models import Subject, Course, Module
+
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ["title", "slug"]
+    preserve_filters = {"slug": ["title"]}
+
+
+class ModuleInline(admin.StackedInline):
+    model = Module
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ["title", "subject", "created"]
+    list_filter = ["created", "subject"]
+    search_fields = ["title", "overview"]
+    prepopulated_fields = {"slug": ["title"]}
+    inlines = [ModuleInline]
