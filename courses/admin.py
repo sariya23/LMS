b746250424202c563a163d3d578bd86a5a6657e6
sariya <dayzwaitik123@yandex.ru>
2024-07-08 18:05:33 +0300

    fix typo in model setting

--- courses/admin.py
+++ courses/admin.py
@@ -5,7 +5,7 @@ from .models import Subject, Course, Module
 @admin.register(Subject)
 class SubjectAdmin(admin.ModelAdmin):
     list_display = ["title", "slug"]
-    preserve_filters = {"slug": ["title"]}
+    prepopulated_fields = {"slug": ["title"]}
 
 
 class ModuleInline(admin.StackedInline):
