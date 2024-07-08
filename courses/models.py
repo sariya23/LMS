00680d8df0d96aef9ddd0b1e939f4a52efeffeb3
sariya <dayzwaitik123@yandex.ru>
2024-07-08 17:51:54 +0300

    models for course

--- courses/models.py
+++ courses/models.py
@@ -1,1 +1,47 @@
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+from .fields import OrderField
+
+
+class Subject(models.Model):
+    class Meta:
+        ordering = ["title"]
+
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=500, unique=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Course(models.Model):
+    class Meta:
+        ordering = ["-created"]
+
+    owner = models.ForeignKey(
+        User, related_name="courses_created", on_delete=models.CASCADE
+    )
+    subject = models.ForeignKey(
+        Subject, related_name="courses", on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    overview = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Module(models.Model):
+    course = models.ForeignKey(Course, related_name="modules", on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    order = OrderField(blank=True, for_fields=["course"])
+
+    class Meta:
+        ordering = ["order"]
+
+    def __str__(self):
+        return f"{self.order}. {self.title}"
